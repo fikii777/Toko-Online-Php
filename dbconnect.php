579841de58057bb968646105ceb9cf8c3cf45c97
zed <121583211+fikii777@users.noreply.github.com>
2023-03-02 18:53:21 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","tokopekita");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
